f3606b6e72964fd9410bb9ce67032e27ea32094b
Jason Axelson <jason.axelson@gmail.com>
2024-08-25 13:24:13 -0500

    Remove phoenix_ecto

--- mix.exs
+++ mix.exs
@@ -33,7 +33,6 @@ defmodule TorchRepro.MixProject do
   defp deps do
     [
       {:phoenix, "~> 1.7.14"},
-      {:phoenix_ecto, "~> 4.5"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 4.1"},
